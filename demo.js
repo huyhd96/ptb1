bd74d6a68a50aad0a4c8f758c716f6f18eb937f9
huyhd96 <huychelsea0@gmail.com>
2021-05-13 09:33:42 -0400

    final

--- demo.js
+++ demo.js
@@ -1,9 +1,11 @@
 let a = +prompt('nhập a: ')
 let b = +prompt('nhập b: ')
 if (a == 0) {
-    alert(" pt vô nghiệm")
-} else if (a === 0 && b === 0) {
-    alert("pt có vỗ số nghiệm")
+    if (b == 0) {
+        alert("pt có vỗ số nghiệm")
+    } else {
+        alert("pt vô nghiệm")
+    }
 } else {
     c = -b / a
     alert("pt có nghiệm là:" + c)
